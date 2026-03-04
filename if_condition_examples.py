ef1bcbc6a2be12a4b477ec3c4ffbd28705ab5255
yash-1199 <yashwanth221226@gmail.com>
2026-03-04 18:53:47 +0530

    Create if_condition_examples.py

---
+++ if_condition_examples.py
@@ -0,0 +1,52 @@
+
+#ATM
+# bal=5000
+# with_draw=10000
+# if with_draw>bal:
+#     print('insufficient balance')
+# else:
+#     bal=bal-with_draw
+#     print('please collect your cash')
+#     print('remaining balance:',bal)
+
+
+# marks=35
+
+# if marks>=90:
+#     print('grade A')
+# elif marks>=75:
+#     print('grade B')
+# elif marks>=50:
+#     print('grade C')
+# else:
+#     print('Fail')
+
+# print('exam results')
+
+#login 
+#check login access
+# if username is not valid tell user that invalid uername
+# if password id not valid tell user that invalid password
+#if evrything is ok tell him login successful
+#`user`
+username='admin3'
+password='admin1234'
+
+# if username=='admin' and password=='admin123':
+#     print('login successful')
+# elif username!='admin':
+#     print('invalid username')
+# else:
+#     password!='admin123'
+#     print('invalid password')
+
+if username!='admin':
+    print('invalid user name')
+elif password!='admin123':
+    print('invalid password')
+else:
+    print('login successful')
+
+
+
+
