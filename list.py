f2c7d6896fef075ea319b2206e841eddacb8abd6
yash-1199 <yashwanth221226@gmail.com git config --list>
2026-03-06 20:36:27 +0530

    list assigment problems

---
+++ list.py
@@ -0,0 +1,79 @@
+#I want to convert all the elements present in list to capital letters
+names=['ram','sita','kumar','raju']
+result=[]
+#s='ram'
+#print(s.upper())
+names[0].upper()
+names[1].upper()
+names[2].upper()
+names[3].upper()
+
+names=['ram','sita','kumar','raju']
+result=[]
+#4 elements
+
+# for name in names:
+#     uppername=name.upper()#RAM,SITA,KUMAR,RAJU
+#     result.append(uppername)
+# print(result)
+
+#name=ram.upper()
+['RAM','SITA','KUMAR','RAJU']
+# I want to multiply each value in the list with 10
+# nums=[2,3,4,5]
+# res=[]
+# for i in nums:
+#     res.append(i*10)
+# print(res)
+
+#I want the sum of all elements in the list
+# nums=[5,10,15,50]
+# sum=0
+# for num in nums:
+#     sum=sum+num
+print(sum)
+
+#sum=0
+#sum=0+5=5
+#sum=5+10=15
+#sum=15+15=30
+#sum=30+50=80
+# i want to find the number of even numbers 
+# I want to print all even numbers as list
+nums=[11,52,87,68,90,21]
+#count=3
+#11%2==0 (F)
+#52%2==0(T)
+#87%2==0(F)
+#68%2==0(T)
+#90%2==0(T)
+#21%2==0(F)
+
+count=0
+for num in nums:
+    if num%2==0:
+        count=count+1
+print(count)
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
